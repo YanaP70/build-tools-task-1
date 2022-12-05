563fbf645f5637aea49faa4b451285f69ae05c95
Yana Podoliak <yana.podolyak@gmail.com>
2022-12-05 13:13:49 +0200

    Build Tools Task1 : initial commit

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,5 @@
+public class StringUtils {
+    public static boolean isPositiveNumber(String str) {
+        return org.apache.commons.lang3.StringUtils.isNumeric(str);
+    }
+}
---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringUtilsTest {
+    @Test
+    void isPositiveNumberTest() {
+        assertTrue(StringUtils.isPositiveNumber("123"));
+        assertFalse(StringUtils.isPositiveNumber("-123"));
+    }
+}
